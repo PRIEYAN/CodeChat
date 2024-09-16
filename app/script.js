5e2e649b31cabe46a7c2a1629b98cb3733f5cd59
PRIEYAN MN <88473323+PRIEYAN@users.noreply.github.com>
2024-09-16 17:33:56 +0530

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,8 @@
+function usr_name(name){
+    document.getElementById('usrname').innerHTML='Hi '+ name;
+}
+function usr_code(code){
+    document.getElementById('usrcode').innerHTML='YOUR CODE: '+ code;
+}
+usr_name()
+
