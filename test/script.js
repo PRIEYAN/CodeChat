aa679a6e27a425e828553a6bbb7222ae1ece5f5a
PRIEYAN MN <88473323+PRIEYAN@users.noreply.github.com>
2024-09-16 17:29:45 +0530

    Delete test directory

--- test/script.js
+++
@@ -1,8 +0,0 @@
-function usr_name(name){
-    document.getElementById('usrname').innerHTML='Hi '+ name;
-}
-function usr_code(code){
-    document.getElementById('usrcode').innerHTML='YOUR CODE: '+ code;
-}
-usr_name()
-
